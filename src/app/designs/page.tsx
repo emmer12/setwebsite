55638d61539acc71bb3c90db6bba4171eabba9cd
emmer12 <laffbaze@gmail.com>
2023-05-13 08:53:34 +0100

    design page setup

--- src/app/designs/page.tsx
+++ src/app/designs/page.tsx
@@ -93,8 +93,8 @@ const Backdrops = () => {
                   </div>
                 </div>
 
-                <div className="em__main__body em__grid__3">
-                  <div className="em__backdrop_card">
+                <div className="em__main__body em_backdrop__grid">
+                  <div className="em__backdrop_card grid">
                     <div className="inner">
                       <div className="display">
                         <img src="./assets/images/b1.png" alt="" />
@@ -125,7 +125,7 @@ const Backdrops = () => {
                     </div>
                   </div>
 
-                  <div className="em__backdrop_card">
+                  <div className="em__backdrop_card grid">
                     <div className="inner">
                       <div className="display">
                         <img src="./assets/images/b1.png" alt="" />
@@ -156,7 +156,38 @@ const Backdrops = () => {
                     </div>
                   </div>
 
-                  <div className="em__backdrop_card">
+                  <div className="em__backdrop_card grid">
+                    <div className="inner">
+                      <div className="display">
+                        <img src="./assets/images/b1.png" alt="" />
+                      </div>
+                      <div className="details">
+                        <div>
+                          <h4>Butterfly Winds</h4>
+                          <h4>Backdrops</h4>
+                        </div>
+                        <div>
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            xmlnsXlink="http://www.w3.org/1999/xlink"
+                            width="99px"
+                            height="18px"
+                          >
+                            <path
+                              fill-rule="evenodd"
+                              fill="rgb(248, 183, 154)"
+                              d="M94.565,11.589 L95.370,17.998 L89.499,15.256 L83.629,17.998 L84.433,11.589 L80.1,6.875 L86.368,5.656 L89.499,0.0 L92.630,5.656 L98.998,6.875 L94.565,11.589 ZM75.370,17.998 L69.499,15.256 L63.629,17.998 L64.433,11.589 L60.1,6.875 L66.368,5.656 L69.499,0.0 L72.630,5.656 L78.998,6.875 L74.565,11.589 L75.370,17.998 ZM55.370,17.998 L49.499,15.256 L43.629,17.998 L44.433,11.589 L40.1,6.875 L46.368,5.656 L49.499,0.0 L52.630,5.656 L58.998,6.875 L54.565,11.589 L55.370,17.998 ZM35.370,17.998 L29.499,15.256 L23.629,17.998 L24.433,11.589 L20.1,6.875 L26.368,5.656 L29.499,0.0 L32.630,5.656 L38.998,6.875 L34.565,11.589 L35.370,17.998 ZM15.370,17.998 L9.499,15.256 L3.629,17.998 L4.433,11.589 L0.1,6.875 L6.368,5.656 L9.499,0.0 L12.630,5.656 L18.998,6.875 L14.565,11.589 L15.370,17.998 Z"
+                            />
+                          </svg>
+                        </div>
+                        <div className="price">
+                          ADD 490 <span>$777</span>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div className="em__backdrop_card grid">
                     <div className="inner">
                       <div className="display">
                         <img src="./assets/images/b1.png" alt="" />
