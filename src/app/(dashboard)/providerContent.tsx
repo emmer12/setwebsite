b5c1cc0b80c6ec47ef6693229293a085433d6b68
emmer12 <laffbaze@gmail.com>
2023-09-26 11:08:10 +0100

    fix

---
+++ src/app/(dashboard)/providerContent.tsx
@@ -0,0 +1,32 @@
+"use client";
+import SideBar from "@/components/dashboard/SideBar";
+import Header from "@/components/dashboard/Header";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import { NotificationContainer } from "react-notifications";
+
+type Props = {
+  children?: React.ReactNode;
+};
+
+export const ContentWrapper = ({ children }: Props) => {
+  const { data: session } = useSession();
+  const router = useRouter();
+
+  if (session?.user.role !== "ADMIN") {
+    router.push("/");
+  }
+
+  return (
+    <div className="flex h-screen bg-gray-50 dark:bg-gray-900 false">
+      <SideBar />
+      <div className="flex flex-col flex-1 w-full">
+        <Header />
+        <main className="h-full overflow-y-auto">
+          <div className="p-5 container">{children}</div>
+        </main>
+      </div>
+      <NotificationContainer />
+    </div>
+  );
+};
--- src/app/(dashboard)/providers.tsx
+++ src/app/(dashboard)/providers.tsx
@@ -1,10 +1,7 @@
 "use client";
-import Header from "@/components/dashboard/Header";
-import SideBar from "@/components/dashboard/SideBar";
 import { SessionProvider, useSession } from "next-auth/react";
-import { NotificationContainer } from "react-notifications";
 import React from "react";
-import { useRouter } from "next/navigation";
+import { ContentWrapper } from "./providerContent";
 
 type Props = {
   children?: React.ReactNode;
@@ -16,28 +13,6 @@ const Provider = ({ children }: Props) => {
       <SessionProvider>
         <ContentWrapper>{children}</ContentWrapper>
       </SessionProvider>
-    </div>
-  );
-};
-
-const ContentWrapper = ({ children }: Props) => {
-  const { data: session } = useSession();
-  const router = useRouter();
-
-  if (session?.user.role !== "ADMIN") {
-    router.push("/");
-  }
-
-  return (
-    <div className="flex h-screen bg-gray-50 dark:bg-gray-900 false">
-      <SideBar />
-      <div className="flex flex-col flex-1 w-full">
-        <Header />
-        <main className="h-full overflow-y-auto">
-          <div className="p-5 container">{children}</div>
-        </main>
-      </div>
-      <NotificationContainer />
     </div>
   );
 };
